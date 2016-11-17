b7ddcca4639c7c3928de26c9421246dda26413d3
KnightWu <wulijian722@gmail.com>
2016-11-17 17:15:44 +0800

    MOD: 支持模板文件夹预设值

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 var path = require("path");
-function KtemplatePlugin(options) {
+var options = {};
+function KtemplatePlugin(_options) {
+    options = _options;
 }
 KtemplatePlugin.prototype.apply = function (compiler) {
+    var templateDir = options.templateDir || 'src/template';
     compiler.plugin("normal-module-factory", function (nmf) {
         nmf.plugin("before-resolve", function (data, callback) {
             if (data.request.indexOf('.kt') > -1 || data.request.indexOf('.nkt') > -1) {
                 var request = data.request;
-                var loader = data.request.indexOf('.kt') > -1 ? 'kt!' : 'babel!kt!';
+                var loader = data.request.indexOf('.kt') > -1 ? 'kt' : 'babel!kt';
                 var loaderAndName = request.replace(/\.kt$|\.nkt$/, '');
-                data.request = loader + path.join(process.cwd(), './src/template/' + loaderAndName);
+                data.request = loader + '?' + templateDir + '!' + path.join(process.cwd(), templateDir + '/' + loaderAndName);
             }
             return callback(null, data);
         });

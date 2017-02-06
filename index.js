4027b6bc09ef8affcb0731cff7c7567862a55ec0
KnightWu <wulijian722@gmail.com>
2017-02-06 14:58:28 +0800

    MOD:  webpack2不再支持简写

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ KtemplatePlugin.prototype.apply = function (compiler) {
         nmf.plugin("before-resolve", function (data, callback) {
             if (data.request.indexOf('.kt') > -1 || data.request.indexOf('.nkt') > -1) {
                 var request = data.request;
-                var loader = data.request.indexOf('.kt') > -1 ? 'kt' : 'babel!kt';
+                var loader = data.request.indexOf('.kt') > -1 ? 'kt-loader' : 'babel-loader!kt-loader';
                 var loaderAndName = request.replace(/\.kt$|\.nkt$/, '');
                 data.request = loader + '?' + templateDir + '!' + path.join(process.cwd(), templateDir + '/' + loaderAndName);
             }
